a158bd42d61b0760993cbcc7857411a1f5d93a35
SKYNET\ggh_ml <ggh228@mustg.kr>
2022-07-04 18:54:03 +0900

    working on section1

--- itsme.js
+++ itsme.js
@@ -6,3 +6,24 @@ const firstTab = new bootstrap.Tab(firstTabEl)
 
 firstTab.show()
 */
+
+
+(()=> {
+    document.querySelector('.navbar-toggler').addEventListener('click',() => {        
+        let navBtn = document.querySelector('.navbar-collapse')
+        let status = navBtn.style.display
+        navBtn.style.display = (status == 'block') ? 'none' : 'block'
+        
+        if (navBtn.style.display == 'block') {
+            // toggled
+            document.getElementById('toggle-btn').setAttribute('src','./resources/button_nav_toggle_x.png');
+            document.getElementById('nav_container').setAttribute('style','background-color: #ffffff; background-image: url("./resources/nav_bg__toggled.png");');
+            document.getElementById('nav_inner_container').setAttribute('style','position: relative; margin-top: 50px;');
+        } else {
+            // default
+            document.getElementById('toggle-btn').setAttribute('src','./resources/button_nav_toggle.png');
+            document.getElementById('nav_container').setAttribute('style','background-color: transparent; background-image: none');
+            document.getElementById('nav_inner_container').setAttribute('style','position: absolute; margin-top: 0px; right: 0px;');
+        }
+    })
+})()
